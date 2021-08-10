0e8d78f91161933af693cfe846a64301a29ef14b
lyric13579 <paixiaoxing1459@163.com>
2021-08-10 09:26:44 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>当前页面不存在，请检查网址是否正确！</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+      <!--   还可以用 a 标签达到这个效果   -->
+      <!--      <a href="#/">返回首页</a>-->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
