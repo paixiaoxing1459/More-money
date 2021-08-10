28822088492195da342fe19ef675015f8ac252cb
lyric13579 <paixiaoxing1459@163.com>
2021-08-10 10:09:48 +0800

    Money 页面使用 Flex 布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,13 +1,10 @@
 <template>
-  <div>
-    <hr/>
-    <div>
-      <router-link to="/money">记账</router-link>
-      |
-      <router-link to="/labels">标签</router-link>
-      |
-      <router-link to="/statistics">导航</router-link>
-    </div>
+  <div class="nav">
+    <router-link to="/money">记账</router-link>
+    |
+    <router-link to="/labels">标签</router-link>
+    |
+    <router-link to="/statistics">导航</router-link>
   </div>
 </template>
 
@@ -18,5 +15,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav{border: 1px solid red}
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,108 @@
 <template>
-  <div>
-    money.vue
-    <Nav />
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+      <p>money.vue</p>
+    </div>
+    <Nav/>
   </div>
 </template>
 
@@ -12,5 +113,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid darkblue;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  // 尽量将所有高度给 content
+  flex-grow: 1;
+  // overflow 定义当一个元素的内容太大而无法适应 块级格式化上下文 时候该做什么
+  // auto 是指 由浏览器定夺，如果内容被修剪，就会显示滚动条
+  overflow: auto;
+}
 </style>
