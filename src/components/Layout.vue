2346d01c6170d564527c39cb1dad7d73a42761b3
lyric13579 <paixiaoxing1459@163.com>
2021-08-10 11:27:21 +0800

    减少重复，创建了 <Layout/> 组件

---
+++ src/components/Layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="nav-wrapper">
+    <div class="content">
+      <!--   插槽，外面传进来东西就放到这里   -->
+      <slot/>
+    </div>
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Layout'
+};
+</script>
+
+
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid darkblue;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.content {
+  // 尽量将所有高度给 content
+  flex-grow: 1;
+  // overflow 定义当一个元素的内容太大而无法适应 块级格式化上下文 时候该做什么
+  // auto 是指 由浏览器定夺，如果内容被修剪，就会显示滚动条
+  overflow: auto;
+}
+</style>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,7 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-      labels.vue
-    </div>
-    <Nav/>
-  </div>
+  <Layout>
+    labels.vue
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -12,20 +9,3 @@ export default {
   name: 'Labels',
 };
 </script>
-
-<style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid darkblue;
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-}
-
-.content {
-  // 尽量将所有高度给 content
-  flex-grow: 1;
-  // overflow 定义当一个元素的内容太大而无法适应 块级格式化上下文 时候该做什么
-  // auto 是指 由浏览器定夺，如果内容被修剪，就会显示滚动条
-  overflow: auto;
-}
-</style>
