d028b3b6215530053cb9f25b0e53a8801db20bb6
lyric13579 <paixiaoxing1459@163.com>
2021-08-11 20:24:24 +0800

    使用 router-link 的属性 active-class ，将导航栏被点击的图标变红

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,15 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+<!--  active-class 是 router-link（vue3 路由的一个组件） 的一个属性，它用于设置 链接激活时使用的 CSS 类名 -->
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计</router-link>
   </nav>
@@ -48,6 +49,10 @@ export default {
         width: 32px;
         height: 32px;
       }
+    }
+
+    > .item.selected{
+      color: red;
     }
   }
 </style>
