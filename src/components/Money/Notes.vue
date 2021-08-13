bbce899f51006c95af9150f6cd7ed564a2bca56f
lyric13579 <paixiaoxing1459@163.com>
2021-08-13 09:51:16 +0800

    完成 money 组件的模块化

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <!--   placeholder 是 input 的一个占位符     -->
+      <input type="text" placeholder="输入备注" />
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Notes'
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
