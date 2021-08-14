144da666f10b135f35adbfa77eccc8a6de126b38
lyric13579 <paixiaoxing1459@163.com>
2021-08-14 21:01:50 +0800

    添加 createAt 字段（保存日期）

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -21,46 +21,56 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component,Prop} from 'vue-property-decorator';
+import Vue from 'vue';
+import {Component, Prop} from 'vue-property-decorator';
 
-  @Component
-  export default class NumberPad extends  Vue{
-    output='0';
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    inputContent(event:MouseEvent){
-      const button = (event.target as HTMLButtonElement);
-      const input = button.textContent as string;
-      if(this.output.length === 16){ return;}
-      if(this.output === '0'){
-        if('0123456789'.indexOf(input)>=0){
-          this.output=input;
-        }else{
-          this.output += input;
-        }
-        return;
-      }
-      if (this.output.indexOf('.')>=0 && input === '.'){return;}
+@Component
+export default class NumberPad extends Vue {
+  output = '0';
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  inputContent(event: MouseEvent) {
+    const button = (event.target as HTMLButtonElement);
+    const input = button.textContent as string;
+    if (this.output.length === 16) { return;}
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(input) >= 0) {
+        this.output = input;
+      } else {
         this.output += input;
-    }
-    remove(){
-      if(this.output.length === 1){
-        this.output = '0';
-      }else{
-        this.output = this.output.slice(0,-1);
       }
+      return;
     }
-    clear(){ this.output = '0' }
-    ok(){
-      this.$emit('update:value',this.output)
-      this.$emit('submit',this.output)
+    if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+    this.output += input;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  remove() {
+    if (this.output.length === 1) {
+      this.output = '0';
+    } else {
+      this.output = this.output.slice(0, -1);
     }
   }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  clear() { this.output = '0'; }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  ok() {
+    this.$emit('update:value', this.output);
+    this.$emit('submit', this.output);
+    // 重置为 0
+    this.output = '0';
+  }
+}
 
 </script>
 
 <style lang="scss" scoped>
 @import "../../assets/style/helper";
+
 .numberPad {
   .output {
     @extend %clearFix;
@@ -71,47 +81,59 @@
     text-align: right;
     height: 72px;
   }
+
   .buttons {
     @extend %clearFix;
+
     > button {
       width: 25%;
       height: 64px;
       float: left;
       background: transparent;
       border: none;
+
       &.ok {
         height: 64px * 2;
         float: right;
       }
+
       &.zero {
         width: 25% * 2;
       }
+
       $bg: #f2f2f2;
+
       &:nth-child(1) {
         background: $bg;
       }
+
       &:nth-child(2),
       &:nth-child(5) {
         background: darken($bg, 5%);
       }
+
       &:nth-child(3),
       &:nth-child(6),
       &:nth-child(9) {
         background: darken($bg, 5% * 2);
       }
+
       &:nth-child(4),
       &:nth-child(7),
       &:nth-child(10) {
         background: darken($color: $bg, $amount: 5% * 3);
       }
+
       &:nth-child(8),
       &:nth-child(11),
       &:nth-child(13) {
         background: darken($color: $bg, $amount: 5% * 4);
       }
+
       &:nth-child(14) {
         background: darken($color: $bg, $amount: 5% * 5);
       }
+
       &:nth-child(12) {
         background: darken($color: $bg, $amount: 5% * 8);
       }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -16,11 +16,14 @@ import Types from '@/components/Money/Types.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Notes from '@/components/Money/Notes.vue';
 
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+
 type Record = {
   tags: string[]
   notes: string
   type: string
   amount: number
+  createdAt?: Date // 类 ， 构造函数
 }
 
 @Component({
@@ -28,35 +31,41 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行', '彩票'];
-  recordList: Record[] = [];
+  recordList: Record[] = recordList;
   record: Record = {
     tags: [],
     notes: '',
-    type: '爱雅淇',
+    type: '-',
     amount: 0
   };
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
 
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   onUpdateAmount(value: string) {
     this.record.amount = parseFloat(value);
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   saveRecord() {
     // 深拷贝一下
-    const record2 = JSON.parse(JSON.stringify(this.record));
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date();
     this.recordList.push(record2);
     console.log(this.recordList);
   }
 
   @Watch('recordList')
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   onRecordListChange() {
     window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
   }
