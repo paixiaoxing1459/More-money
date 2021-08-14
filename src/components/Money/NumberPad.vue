7f6b45221f260eb6dc0d7961ef0714e7f552df25
lyric13579 <paixiaoxing1459@163.com>
2021-08-14 10:51:00 +0800

    大致完成 NumberPad 组件（还剩下 ok 的函数没有写）

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -5,16 +5,16 @@
       <button @click="inputContent">1</button>
       <button @click="inputContent">2</button>
       <button @click="inputContent">3</button>
-      <button>删除</button>
+      <button @click="remove">删除</button>
       <button @click="inputContent">4</button>
       <button @click="inputContent">5</button>
       <button @click="inputContent">6</button>
-      <button>清空</button>
+      <button @click="clear">清空</button>
       <button @click="inputContent">7</button>
       <button @click="inputContent">8</button>
       <button @click="inputContent">9</button>
-      <button class="ok">ok</button>
-      <button class="zero" @click="inputContent">0</button>
+      <button @click="ok" class="ok">ok</button>
+      <button @click="inputContent" class="zero">0</button>
       <button @click="inputContent">.</button>
     </div>
   </div>
@@ -42,6 +42,15 @@
       if (this.output.indexOf('.'>=0) && input === '.'){return;}
         this.output += input;
     }
+    remove(){
+      if(this.output.length === 1){
+        this.output = '0';
+      }else{
+        this.output = this.output.slice(0,-1);
+      }
+    }
+    clear(){ this.output = '0' }
+    ok(){}
   }
 
 </script>
