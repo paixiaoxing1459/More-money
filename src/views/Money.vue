91491a7adbb2916d75bb9ff70ee4acdc8131cebd
lyric13579 <paixiaoxing1459@163.com>
2021-08-17 23:14:58 +0800

    解决 id 生成问题

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,36 +1,34 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
-    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
+    <Types :value.sync="record.type" />
     <div class="notes">
-      <FormItem field-name="备注"
-                placeholder="在这里添加备注"
-                @update:value="onUpdateNotes"
+      <FormItem
+        field-name="备注"
+        placeholder="在这里添加备注"
+        @update:value="onUpdateNotes"
       />
     </div>
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layout>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import {Component, Watch} from 'vue-property-decorator';
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import Tags from '@/components/Money/Tags.vue';
-import FormItem from '@/components/Money/FormItem.vue';
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import Vue from "vue";
+import { Component, Watch } from "vue-property-decorator";
+import NumberPad from "@/components/Money/NumberPad.vue";
+import Types from "@/components/Money/Types.vue";
+import Tags from "@/components/Money/Tags.vue";
+import FormItem from "@/components/Money/FormItem.vue";
+import recordListModel from "@/models/recordListModel";
+import tagListModel from "@/models/tagListModel";
 
 // eslint-disable-next-line no-undef
 const recordList = recordListModel.fetch();
 const tagList = tagListModel.fetch();
 
-
 @Component({
-  components: {FormItem, Tags, Types, NumberPad}
+  components: { FormItem, Tags, Types, NumberPad },
 })
 export default class Money extends Vue {
   tags = tagList;
@@ -39,9 +37,9 @@ export default class Money extends Vue {
   // eslint-disable-next-line no-undef
   record: RecordItem = {
     tags: [],
-    notes: '',
-    type: '-',
-    amount: 0
+    notes: "",
+    type: "-",
+    amount: 0,
   };
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -53,7 +51,6 @@ export default class Money extends Vue {
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
-
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   onUpdateAmount(value: string) {
@@ -70,7 +67,7 @@ export default class Money extends Vue {
     console.log(this.recordList);
   }
 
-  @Watch('recordList')
+  @Watch("recordList")
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   onRecordListChange() {
     recordListModel.save(this.recordList);
@@ -84,7 +81,7 @@ export default class Money extends Vue {
   flex-direction: column-reverse;
 }
 
-.notes{
+.notes {
   padding: 12px 0;
 }
 </style>
