a02e817ad46be8b3f059e57c7ef5561611cf41c0
lyric13579 <paixiaoxing1459@163.com>
2021-08-17 21:00:03 +0800

    可以编辑和删除标签

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,12 +1,13 @@
 <template>
-  <svg class="icon">
+  <svg class="icon" @click="$emit('click',$event)">
     <!--  这里的 v-bind 可以省略，但是我不想省略，怕自己看不到  -->
-    <use v-bind:xlink:href="'#'+name"/>
+    <use :xlink:href="'#'+name"/>
   </svg>
 </template>
 
 <script lang="ts">
 // 将 icons 目录里面所有的的 svg 文件引入
+// eslint-disable-next-line no-undef
 let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {
   importAll(require.context('../assets/icons', true, /\.svg$/));
