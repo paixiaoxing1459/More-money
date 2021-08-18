5459e281a5378deaf58ad1890ea3355fa2efeeb3
lyric13579 <paixiaoxing1459@163.com>
2021-08-18 11:21:08 +0800

    重新封装 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,19 +1,22 @@
 const localStorageKeyName = 'recordList';
-const recordListModel = {
 
+const recordListModel = {
+  data: [] as RecordItem[],
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  clone(data:RecordItem[] | RecordItem) {
+  clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
   },
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   fetch() {
-    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.data;
   },
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  save() {
+    window.localStorage.setItem(localStorageKeyName,
+      JSON.stringify(this.data));
   }
 };
 
