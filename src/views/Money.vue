20d92dbfd73ed20a33256036d80d5a836bea3aca
lyric13579 <paixiaoxing1459@163.com>
2021-10-28 19:46:13 +0800

    模块化 intervalList.ts 和 recordTypeList.ts, 在 Money.vue 里使用 Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,9 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
-    <Types :value.sync="record.type" />
+    <Tabs :data-source="recordTypeList"
+          :value.sync="record.type"
+    />
     <div class="notes">
       <FormItem
         field-name="备注"
@@ -20,14 +22,18 @@ import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Tags from "@/components/Money/Tags.vue";
 import FormItem from "@/components/Money/FormItem.vue";
+import Tabs from '@/components/Tabs.vue';
+import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: { FormItem, Tags, Types, NumberPad },
+  components: {Tabs, FormItem, Tags, Types, NumberPad },
 })
 export default class Money extends Vue {
   get recordList(){
     return this.$store.state.recordList
   }
+
+  recordTypeList= recordTypeList;
 
   // eslint-disable-next-line no-undef
   record: RecordItem = {
