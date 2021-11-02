f7ab0d33b3db339f9bd552dd26257eb9fcb1e411
lyric13579 <paixiaoxing1459@163.com>
2021-11-02 17:01:33 +0800

    完成列表的 CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,25 +2,43 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval" height="48px"/>
-    <div>
-      type: {{ type }}
-      <br/>
-      interval: {{ interval }}
-    </div>
-    <div>
-      <ol>
-        <li v-for="(group,index) in result" :key="index">
-          <h3>{{group.title}}</h3>
-          <ol>
-            <li v-for="item in group.items" :key="item.id">
-              {{ item.amount }} {{ item.createdAt }}
-            </li>
-          </ol>
-        </li>
-      </ol>
-    </div>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
+<style scoped lang="scss">
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.title {
+  @extend %item;
+}
+
+.record {
+  background: white;
+  @extend %item;
+}
+.notes{
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999999;
+}
+</style>
 
 <script lang="ts">
 import Vue from 'vue';
@@ -33,6 +51,11 @@ import recordTypeList from '@/constants/recordTypeList';
   components: {Tabs},
 })
 export default class Statistics extends Vue {
+  // eslint-disable-next-line no-undef
+  tagString(tags:Tag[]) {
+    return tags.length ===0 ? '无' : tags.join(',');
+  }
+
   get recordList() {
     // eslint-disable-next-line no-undef
     return (this.$store.state as RootState).recordList;
