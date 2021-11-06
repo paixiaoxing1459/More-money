1ddf39080d745c5a6294b26ba071fcd9cd414712
lyric13579 <paixiaoxing1459@163.com>
2021-11-06 11:50:32 +0800

    修复一进页面就弹出提示的 bug

--- src/mixins/TagHelper.ts
+++ src/mixins/TagHelper.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
+const map:{[key:string]:string} = {
+  'tag name duplicated':'标签名重复了~'
+}
+
 @Component
 export class TagHelper extends Vue {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
@@ -8,6 +12,10 @@ export class TagHelper extends Vue {
     const name = window.prompt('请输入标签名');
     if (!name) {return window.alert('标签名不能为空');}
     this.$store.commit( 'createTag', name);
+    if(this.$store.state.createTagError){
+      if(this.$store.state.createTagError.message === 'tag name duplicated')
+      window.alert(map[this.$store.state.createTagError.message] || "未知错误");
+    }
   }
 }
 
